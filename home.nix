0574c986b60130b958a728d2f8257885a17581cd
Shreeram Modi <smodi@smodi.net>
2024-10-17 13:44:09 -0400

    zsh: Add yazi autocd function

--- home.nix
+++ home.nix
@@ -189,6 +189,15 @@
               rm -f "$NNN_TMPFILE" > /dev/null
           }
         }
+
+      function y() {
+          local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+          yazi "$@" --cwd-file="$tmp"
+          if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+              builtin cd -- "$cwd"
+          fi
+          rm -f -- "$tmp"
+      }
     '';
 
     plugins = [
