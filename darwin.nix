cbae114ba5c8181a177f8ca1fcc6c3007580573d
Shreeram Modi <smodi@smodi.net>
2024-10-17 13:37:10 -0400

    darwin: Cleanup system packages

--- darwin.nix
+++ darwin.nix
@@ -5,8 +5,7 @@
 {
     # List packages installed in system profile. To search by name, run:
     # $ nix-env -qaP | grep wget
-    environment.systemPackages =
-    [ pkgs.vim ];
+    environment.systemPackages = [ ];
 
     # Auto upgrade nix package and the daemon service.
     services.nix-daemon.enable = true;
