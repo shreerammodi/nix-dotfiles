bb9a309b93fc3519d7786b14bcf71b5d499b908e
Shreeram Modi <smodi@smodi.net>
2024-10-17 13:37:50 -0400

    darwin: Replace yabai with aerospace

--- darwin.nix
+++ darwin.nix
@@ -27,54 +27,24 @@
     security.pam.enableSudoTouchIdAuth = true;
 
     # The platform the configuration will be used on.
-    nixpkgs.hostPlatform = "aarch64-darwin";
-
-    services.yabai = {
-        enable = true;
-        package = pkgs.yabai;
-        enableScriptingAddition = true;
+    nixpkgs = {
+        hostPlatform = "aarch64-darwin";
         config = {
-                mouse_follows_focus = "off";
-                focus_follows_mouse = "off";
-                window_origin_display = "default";
-                window_placement = "second_child";
-                window_shadow = "off";
-                window_opacity = "off";
-                window_opacity_duration = 0.0;
-                active_window_opacity = 1.0;
-                normal_window_opacity = 0.90;
-                insert_feedback_color = "0xFFCCA37E";
-                split_ratio = 0.50;
-                split_type = "auto";
-                auto_balance = "off";
-                mouse_modifier = "ctrl";
-                mouse_action1 = "move";
-                mouse_action2 = "resize";
-                mouse_drop_action = "stack";
-                window_animation_duration = 0.0;
-                window_animation_frame_rate = 120;
-
-                layout = "bsp";
-                top_padding = 5;
-                bottom_padding = 5;
-                left_padding = 5;
-                right_padding = 5;
-                window_gap = 5;
+            allowUnfree = true;
         };
-        extraConfig = ''
-            yabai -m rule --add app="^Disk Utility$" manage=off
-            yabai -m rule --add app="^Flux$" manage=off
-            yabai -m rule --add app="^Raycast$" manage=off
-            yabai -m rule --add app="^Discord*" space=last
-            yabai -m rule --add app="^Emacs$" manage=on
-            yabai -m rule --add app="^Steam$" manage=off
-            yabai -m rule --add app="^Finda$" manage=off
-        '';
     };
 
-    services.skhd = {
-        enable = true;
-        package = pkgs.skhd;
+    homebrew = {
+      enable = true;
+
+      brews = [
+        "felixkratz/formulae/borders"
+      ];
+
+      casks = [
+        "nikitabobko/tap/aerospace"
+        "raycast"
+      ];
     };
 
     users.users.shreeram = {
