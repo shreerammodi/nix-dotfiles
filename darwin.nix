329fb6dea83da8f510246de8eaa7326fc9a8839d
Shreeram Modi <smodi@smodi.net>
2025-02-27 17:09:34 -0500

    darwin: Format

--- darwin.nix
+++ darwin.nix
@@ -3,52 +3,50 @@
 { pkgs, ... }:
 
 {
-    # List packages installed in system profile. To search by name, run:
-    # $ nix-env -qaP | grep wget
-    environment.systemPackages = [ ];
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = [ ];
 
-    # Auto upgrade nix package and the daemon service.
-    services.nix-daemon.enable = true;
-    # services.karabiner-elements.enable = true;
-    # nix.package = pkgs.nix;
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+  # services.karabiner-elements.enable = true;
+  # nix.package = pkgs.nix;
 
-    # Necessary for using flakes on this system.
-    nix.settings.experimental-features = "nix-command flakes";
+  # Necessary for using flakes on this system.
+  nix.settings.experimental-features = "nix-command flakes";
 
-    # Create /etc/zshrc that loads the nix-darwin environment.
-    programs.zsh.enable = true;  # default shell on catalina
-    # programs.fish.enable = true;
+  # Create /etc/zshrc that loads the nix-darwin environment.
+  programs.zsh.enable = true; # default shell on catalina
+  # programs.fish.enable = true;
 
-    # Used for backwards compatibility, please read the changelog before changing.
-    # $ darwin-rebuild changelog
-    system.stateVersion = 4;
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
 
-    # Enable Touch ID for sudo
-    security.pam.enableSudoTouchIdAuth = true;
+  # Enable Touch ID for sudo
+  security.pam.enableSudoTouchIdAuth = true;
 
-    # The platform the configuration will be used on.
-    nixpkgs = {
-        hostPlatform = "aarch64-darwin";
-        config = {
-            allowUnfree = true;
-        };
+  # The platform the configuration will be used on.
+  nixpkgs = {
+    hostPlatform = "aarch64-darwin";
+    config = {
+      allowUnfree = true;
     };
+  };
 
-    homebrew = {
-      enable = true;
+  homebrew = {
+    enable = true;
 
-      brews = [
-        "felixkratz/formulae/borders"
-      ];
+    brews = [
+    ];
 
-      casks = [
-        "nikitabobko/tap/aerospace"
-        "raycast"
-      ];
-    };
+    casks = [
+      "raycast"
+    ];
+  };
 
-    users.users.shreeram = {
-        name = "shreeram";
-        home = "/Users/shreeram";
-    };
+  users.users.shreeram = {
+    name = "shreeram";
+    home = "/Users/shreeram";
+  };
 }
