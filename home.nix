f6c45a6d853c84b5a143d56af841f6c57d30f0db
Shreeram Modi <smodi@smodi.net>
2025-09-17 17:55:49 -0400

    home: add Ghostty shell integration

--- home.nix
+++ home.nix
@@ -167,6 +167,12 @@
 
       # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
       [[ ! -f ~/.config/zsh/.p10k.zsh ]] || source ~/.config/zsh/.p10k.zsh
+
+
+      # Ghostty shell integration for Bash. This should be at the top of your bashrc!
+      if [ -n "''${GHOSTTY_RESOURCES_DIR}" ]; then
+        builtin source "''${GHOSTTY_RESOURCES_DIR}/shell-integration/bash/ghostty.bash"
+      fi
     '';
 
     completionInit = ''
